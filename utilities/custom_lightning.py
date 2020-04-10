a7332a2d61a7c58c616c36fb4d3ad521ab794a4a
WSzP <peter@kaizen-ux.com>
2020-04-10 19:06:09 +0300

    Updated to PyTorch Lightning 1.7.3

--- utilities/custom_lightning.py
+++ utilities/custom_lightning.py
@@ -1,29 +1,36 @@
-from pytorch_lightning.profiler.profiler import BaseProfiler
+from pytorch_lightning.profiler import BaseProfiler
 from collections import defaultdict
 import time
 import numpy as np
 import pandas as pd
 
 class CSVProfiler(BaseProfiler):
+    """
+    This profiler records the duration of actions. 
+    Optionally it prints and saves them to .CSV format.
+    """
 
-    def __init__(self,output_path=None,verbose=True):
+    def __init__(self, output_path: str = None, verbose: bool = True):
         """
-        :param output_path (str): The path where the profiler will save the resulting .CSV. (Optional)
-        :param verbose (bool): Print the profiler results on screen, using print() 
+        Params:
+            output_path (str): The path where the profiler will save the resulting .CSV. (Optional)
+            param verbose (bool): Print the profiler results on screen, using print() 
         """
-        self.output_path = output_path
-        self.verbose = verbose
         self.current_actions = {}
         self.recorded_durations = defaultdict(list)
+        self.verbose = verbose
+        self.output_path = output_path               
+        streaming_out = None
+        super().__init__(output_streams=streaming_out)
 
-    def start(self, action_name):
+    def start(self, action_name: str) -> None:
         if action_name in self.current_actions:
             raise ValueError(
                 f"Attempted to start {action_name} which has already started."
             )
         self.current_actions[action_name] = time.monotonic()
 
-    def stop(self, action_name):
+    def stop(self, action_name: str) -> None:
         end_time = time.monotonic()
         if action_name not in self.current_actions:
             raise ValueError(
@@ -33,7 +40,13 @@ class CSVProfiler(BaseProfiler):
         duration = end_time - start_time
         self.recorded_durations[action_name].append(duration)
 
+    def summary(self) -> str:        
+        return ""
+
     def describe(self):
+        """Logs a profile report after the conclusion of the training run."""
+        super().describe()
+        
         log = []    
         for action, durations in self.recorded_durations.items():
             log.append([action,np.mean(durations),np.sum(durations)])
@@ -48,4 +61,9 @@ class CSVProfiler(BaseProfiler):
         if len(self.output_path)>0:
             log_df.to_csv(self.output_path)
             if self.verbose:
-                print(f"\nProfiler output saved to: {self.output_path}")
+                print(f"\nProfiler output saved to: {self.output_path}")  
+        
+        
+
+    def __del__(self):
+        """Close profiler's stream."""
